fc1a8efedd31073ed54129e03d852397d79c9070
Ali Murat Taşin <alimurat_tasin@hotmail.com>
2023-01-06 18:01:54 +0300

    character behaviour created

---
+++ Assets/MangoramaStudio/Scripts/Behaviours/CharacterBehaviour.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterBehaviour : MonoBehaviour
+{
+    private bool _isStartedToMove;
+    private float _lerpValue;
+    private Vector3 _spawnPos;
+
+    private void Update()
+    {
+        if (_isStartedToMove == true)
+        {
+            _lerpValue += Time.deltaTime * 2f;
+            transform.localPosition = Vector3.Lerp(transform.localPosition, _spawnPos, _lerpValue);
+        }
+    }
+
+    public void MoveChacartersToTerritory(Vector3 spawnPos)
+    {        
+        _spawnPos = transform.localPosition + spawnPos;
+        _isStartedToMove = true;
+    }
+}
