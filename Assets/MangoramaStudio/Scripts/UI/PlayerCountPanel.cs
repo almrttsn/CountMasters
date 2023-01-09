d527c8ac56d621171b2b1fe44d036ea98a815e99
Ali Murat Taşin <alimurat_tasin@hotmail.com>
2023-01-09 12:24:47 +0300

    count panel referance change

--- Assets/MangoramaStudio/Scripts/UI/PlayerCountPanel.cs
+++ Assets/MangoramaStudio/Scripts/UI/PlayerCountPanel.cs
@@ -6,11 +6,10 @@ using UnityEngine.UI;
 public class PlayerCountPanel : MonoBehaviour
 {
     [SerializeField] private Text _playerCountText;
-    [SerializeField] private PlayerParentBehaviour _playerBehaviour;
+    [SerializeField] private PlayerParentBehaviour _playerParentBehaviour;
 
-    
     private void Update()
     {
-        _playerCountText.text = (" Player Count is: " + _playerBehaviour.PlayerCount);
+        _playerCountText.text = (" Player Count is: " + _playerParentBehaviour.PlayerCount);
     }
 }
