5e898443e59fb58a9dcb6d691c99336541aeed12
Ali Murat Taşin <alimurat_tasin@hotmail.com>
2023-01-11 15:20:02 +0300

    enemy behaviour created

---
+++ Assets/MangoramaStudio/Scripts/Behaviours/EnemyBehaviour.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+
+    private bool _isStartedToMove;
+    private float _lerpValue;
+    private Vector3 _spawnPos;
+
+
+    private void Update()
+    {
+        if (_isStartedToMove == true)
+        {
+            _lerpValue += Time.deltaTime * 2f;
+            transform.localPosition = Vector3.Lerp(transform.localPosition, _spawnPos, _lerpValue);
+        }
+    }
+
+    public void MoveCharactersToTerritory(Vector3 spawnPos)
+    {
+        _spawnPos = spawnPos;
+        _isStartedToMove = true;
+    }
+
+    
+}
